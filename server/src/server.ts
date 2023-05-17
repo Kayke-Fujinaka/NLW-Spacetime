9d61ddf3653f2483e3142eee0f5fb2f9df2149bc
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-17 11:08:14 -0300

    build: setup fastify cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
+
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
 const PORT = 3333
 
+app.register(cors, {
+  origin: true,
+})
 app.register(memoriesRoutes)
 
 app
