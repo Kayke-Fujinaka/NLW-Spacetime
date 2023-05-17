2dc953bdebf6be3c9edc82e94493f5baafa35c1b
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-17 10:52:27 -0300

    feat: creates route that get a memory

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
@@ -18,7 +19,19 @@ export async function memoriesRoutes(app: FastifyInstance) {
     })
   })
 
-  app.get('/memories/:id', async () => {})
+  app.get('/memories/:id', async (request) => {
+    const paramsScheme = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsScheme.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: { id },
+    })
+
+    return memory
+  })
 
   app.post('/memories', async () => {})
 
