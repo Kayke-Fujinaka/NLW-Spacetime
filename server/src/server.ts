71e83ed695d6fd26742b017a52be5510cc6d69d1
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-18 18:58:57 -0300

    config: setup jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
 
 import { authRoutes } from './routes/auth'
@@ -13,6 +14,11 @@ const PORT = 3333
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
