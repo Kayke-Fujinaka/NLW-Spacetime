765134c3f683033dc0746b3379df88558bfdbe84
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-19 11:46:41 -0300

    feat: saves token in secure store

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,15 +4,26 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStorage from 'expo-secure-store'
 import { StatusBar } from 'expo-status-bar'
 import { styled } from 'nativewind'
+import { useEffect } from 'react'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import blurBg from './src/assets/bg-blur.png'
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 import Stripes from './src/assets/stripe.svg'
+import { api } from './src/lib/api/api'
 
 const StyledStripes = styled(Stripes)
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/9fe8b962b9a94443fab4f',
+}
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -20,6 +31,40 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signWithGithub] = useAuthRequest(
+    {
+      clientId: '9fe8b962b9a94443fab4',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      api
+        .post('/register', {
+          code,
+        })
+        .then((response) => {
+          const { token } = response.data
+
+          SecureStorage.setItemAsync('token', token)
+        })
+        .catch((error) => console.error(error))
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -49,6 +94,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-3"
+          onPress={() => signWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
